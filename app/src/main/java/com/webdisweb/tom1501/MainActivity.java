6a7bcfa76dba18a5c91172e98f7f0115877946cd
webdisweb-sudo <webdisweb@gmail.com>
2026-06-30 17:35:58 +0200

    add: app/src/main/java/com/webdisweb/tom1501/MainActivity.java

---
+++ app/src/main/java/com/webdisweb/tom1501/MainActivity.java
@@ -0,0 +1,41 @@
+package com.webdisweb.tom1501;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // не гасить экран во время игры
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.destroy();
+        }
+        super.onDestroy();
+    }
+}
